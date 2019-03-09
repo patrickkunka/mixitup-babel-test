0cbaf896b75ecbbee43f035275235b16f6da2d5a
Patrick Kunka <patrick@kunkalabs.com>
2019-03-09 17:29:54 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import mixitup from 'mixitup';
+import mixitupPagination from './vendor/mixitup-pagination.min';
+
+mixitup.use(mixitupPagination);
+
+const containerEl = document.querySelector('.container');
+const mixer = mixitup(containerEl, {
+    pagination: {
+        limit: 4
+    }
+});
+
+console.log(mixer);
